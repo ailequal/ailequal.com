816c0b43742cb5f8c4e3895d0b5dbd978840e2e9
ailequal <37016865+ailequal@users.noreply.github.com>
2022-05-20 17:20:15 +0200

    Added graphql basic schema.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,7 @@
 module.exports = {
     siteMetadata: {
-        siteUrl: `https://www.yourdomain.tld`
+        siteUrl: `https://www.ailequal.com`,
+        title: `ailequal`
     },
     plugins: [
         "gatsby-plugin-image",
