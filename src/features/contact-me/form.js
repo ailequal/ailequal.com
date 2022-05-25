c335187a6564438a4d6eee25ff4d20364ae55387
ailequal <37016865+ailequal@users.noreply.github.com>
2022-05-25 15:55:51 +0200

    Testing form submission.

--- src/features/contact-me/form.js
+++ src/features/contact-me/form.js
@@ -1,89 +1,94 @@
 import * as React from "react"
 
 const Form = () => {
-    return (<div className="mt-10 sm:mt-0">
-        <div className="mt-5 md:mt-0 md:col-span-2">
+    return (
+        <div className="mt-10 sm:mt-0">
+            <div className="mt-5 md:mt-0 md:col-span-2">
 
-            <form action="#" method="POST">
-                <div className="overflow-hidden sm:rounded-md">
+                <form method="POST" data-netlify="true" netlify-honeypot="bot-field" name="contact-me">
+                    <input type="hidden" name="bot-field"/>
+                    <input type="hidden" name="form-name" value="contact-me"/>
 
-                    <div className="px-4 py-5 bg-white sm:p-6">
-                        <div className="grid grid-cols-6 gap-6">
+                    <div className="overflow-hidden sm:rounded-md">
 
-                            <div className="col-span-12">
-                                <label htmlFor="first-name"
-                                       className="block text-sm font-medium">
-                                    First name
-                                </label>
-                                <input
-                                    type="text"
-                                    name="first-name"
-                                    id="first-name"
-                                    autoComplete="given-name"
-                                    className="mt-1 focus:ring-gray-800 focus:border-gray-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                />
-                            </div>
+                        <div className="px-4 py-5 bg-white sm:p-6">
+                            <div className="grid grid-cols-6 gap-6">
 
-                            <div className="col-span-12">
-                                <label htmlFor="last-name"
-                                       className="block text-sm font-medium">
-                                    Last name
-                                </label>
-                                <input
-                                    type="text"
-                                    name="last-name"
-                                    id="last-name"
-                                    autoComplete="family-name"
-                                    className="mt-1 focus:ring-gray-800 focus:border-gray-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                />
-                            </div>
+                                <div className="col-span-12">
+                                    <label htmlFor="first-name" className="block text-sm font-medium">
+                                        First name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="first-name"
+                                        id="first-name"
+                                        className="mt-1 focus:ring-gray-800 focus:border-gray-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                        required
+                                    />
+                                </div>
 
-                            <div className="col-span-12">
-                                <label htmlFor="email-address"
-                                       className="block text-sm font-medium">
-                                    Email
-                                </label>
-                                <input
-                                    type="text"
-                                    name="email-address"
-                                    id="email-address"
-                                    autoComplete="email"
-                                    className="mt-1 focus:ring-gray-800 focus:border-gray-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
-                                />
-                            </div>
+                                <div className="col-span-12">
+                                    <label htmlFor="last-name" className="block text-sm font-medium">
+                                        Last name
+                                    </label>
+                                    <input
+                                        type="text"
+                                        name="last-name"
+                                        id="last-name"
+                                        className="mt-1 focus:ring-gray-800 focus:border-gray-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                        required
+                                    />
+                                </div>
 
-                            <div className="col-span-12">
-                                <label htmlFor="message" className="block text-sm font-medium">
-                                    Message
-                                </label>
-                                <div className="mt-1">
+                                <div className="col-span-12">
+                                    <label htmlFor="email-address" className="block text-sm font-medium">
+                                        Email
+                                    </label>
+                                    <input
+                                        type="email"
+                                        name="email-address"
+                                        id="email-address"
+                                        autoComplete="email"
+                                        className="mt-1 focus:ring-gray-800 focus:border-gray-500 block w-full shadow-sm sm:text-sm border-gray-300 rounded-md"
+                                        required
+                                    />
+                                </div>
+
+                                <div className="col-span-12">
+                                    <label htmlFor="message" className="block text-sm font-medium">
+                                        Message
+                                    </label>
+                                    <div className="mt-1">
                                     <textarea
                                         id="message"
                                         name="message"
-                                        rows={3}
+                                        rows={5}
                                         className="shadow-sm focus:ring-gray-800 focus:border-gray-500 mt-1 block w-full sm:text-sm border border-gray-300 rounded-md"
-                                        defaultValue={''}
+                                        required
                                     />
+                                    </div>
                                 </div>
+
                             </div>
-
                         </div>
+
+                        <div className="px-4 py-3 text-right sm:px-6">
+                            <div data-netlify-recaptcha="true"></div>
+
+                            <button
+                                type="submit"
+                                className="bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
+                            >
+                                Submit
+                            </button>
+                        </div>
+
                     </div>
+                </form>
 
-                    <div className="px-4 py-3 text-right sm:px-6">
-                        <button
-                            type="submit"
-                            className="bg-gray-700 text-white px-3 py-2 rounded-md text-sm font-medium"
-                        >
-                            Submit
-                        </button>
-                    </div>
-
-                </div>
-            </form>
-
+            </div>
         </div>
-    </div>)
+    )
 }
 
 export default Form
