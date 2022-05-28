7d2d82f757ebbbcd90215561588d263331a1f8f9
ailequal <37016865+ailequal@users.noreply.github.com>
2022-05-28 12:00:54 +0200

    Cleaning.

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,6 +6,12 @@ import Introduction from "../features/home/introduction";
 import Layout from "../shared/layout";
 import Skills from "../features/home/skills";
 
+// TODO: All the data across the whole project should be queried with GraphQL (not hard-coded).
+// TODO: Refactor some components.
+// TODO: Clean up and refactor the styling of the website.
+// TODO: Rewrite the project with TypeScript.
+// TODO: Create a proper 404 page that supports Netlify.
+
 const IndexPage = () => {
     return (
         <Layout pageTitle='Home'>
